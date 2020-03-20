aff2b859624c0ad6ae1ceef5169f275e8d8867ba
marsou001 <marsou431@gmail.com>
2020-03-20 17:20:30 +0100

    without hooks

--- src/App.js
+++ src/App.js
@@ -1,26 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Quotes from './components/quote';
+import Buttons from './components/buttons';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      colors: ["#4287f5", "#42ecf5", "#42f584", "#37d925", "#b3f538", "#d1d11f", "#fc8535", "#fc3535", "#b537f0", "#e635d1", "#8a7f83", "#303030"],
+      quotes: [
+        {
+          text: 'You can observe a lot just by watching.',
+          author: 'Yogi Berra'
+        },
+        {
+          text: 'Nothing happens unless first we dream',
+          author: 'Carl Sandburg'
+        },
+        {
+          text: 'Genius is one percent inspiration and ninety-nine percent perspiration.',
+          author: 'Thomas Edison'
+        }        
+      ],
+      color: 0,
+      quote: 0
+    }
+    this.newQuote = this.newQuote.bind(this)
+  }
+
+  newQuote() {
+    this.setState({
+      color: parseInt(Math.random() * this.state.colors.length),
+      quote: parseInt(Math.random() * this.state.quotes.length)
+    })
+    document.getElementsByTagName("html")[0].style.setProperty("--rand-color", this.state.colors[this.state.color])
+  }
+
+  componentDidMount() {
+    fetch("https://type.fit/api/quotes")
+    .then(response => response.json())
+    .then(responseData => {
+      this.setState({
+        quotes: [...responseData],
+        color: parseInt(Math.random() * this.state.colors.length),
+        quote: parseInt(Math.random() * responseData.length)
+      })
+    })
+    .catch(err => console.log(err, err.data))
+
+    // this.setState({
+    //   color: parseInt(Math.random() * this.state.colors.length),
+    //   quote: parseInt(Math.random() * this.state.quotes.length)      
+    // })
+    document.getElementsByTagName("html")[0].style.setProperty("--rand-color", this.state.colors[this.state.color])
+  }
+
+  render() {
+    return (
+      <main>
+        <Quotes text={this.state.quotes[this.state.quote].text} author={this.state.quotes[this.state.quote].author} />
+        <Buttons text={this.state.quotes[this.state.quote].text} author={this.state.quotes[this.state.quote].author} newQuote={this.newQuote} />
+      </main>
+    )
+  }
 }
 
 export default App;
---
+++ src/components/buttons.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function Buttons(props) {
+    const { text, author, newQuote } = props;
+    const twitterLink = `https://twitter.com/intent/tweet?hashtags=quotes&text="${encodeURIComponent(text)}" ${encodeURIComponent(author)}`
+    const tumblrLink = `https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes&caption=${encodeURIComponent(author)}&content=${encodeURIComponent(text)}&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button`
+
+    return (
+        <div className="footer">
+            <div className="social">
+                <a href={twitterLink} title="share to Twitter" target="_blank" rel="noopener noreferrer"><i className="fab fa-twitter"></i></a>
+                <a href={tumblrLink} title="share to Tumblr" target="_blank" rel="noopener noreferrer"><i className="fab fa-tumblr"></i></a>
+            </div>
+            <div className="new-quote">
+                <button type="button" title="New quote" onClick={newQuote}>New quote</button>
+            </div>
+        </div>
+    )
+}
+
